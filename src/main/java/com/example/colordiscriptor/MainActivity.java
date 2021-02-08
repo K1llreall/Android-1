74ce0f44897aae029632c4e7792c03ec68abe808
Kirill Smolin <74225161+K1llreall@users.noreply.github.com>
2021-02-09 00:33:57 +0500

    Add files via upload

---
+++ src/main/java/com/example/colordiscriptor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.colordiscriptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinnerColors;
+    private TextView textViewDescriptionTemp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinnerColors= findViewById(R.id.spinnerColors);
+        textViewDescriptionTemp= findViewById(R.id.textViewDescriptoinTemp);
+    }
+
+
+    public void showDescription(View view) {
+        int position= spinnerColors.getSelectedItemPosition();
+        String description= getDiscriptionByPosition(position);
+        textViewDescriptionTemp.setText(description);
+
+    }
+    private String getDiscriptionByPosition (int position){
+        String[] descriptions= getResources().getStringArray(R.array.description_of_temp);
+        return descriptions[position];
+    }
+}
